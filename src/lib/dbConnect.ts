e27eb0b7440e3caad141474b2d535c430e627a5f
Naitik Jain <naitikjainjbp@gmail.com>
2025-11-05 09:51:31 +0000

    registration failed again

--- src/lib/dbConnect.ts
+++ src/lib/dbConnect.ts
@@ -1,10 +1,10 @@
 import mongoose from 'mongoose';
 
-const MONGODB_URI = process.env.MONGODB_URI!;
+const MONGODB_URI = process.env.MONGODB_URI;
 
 if (!MONGODB_URI) {
   throw new Error(
-    'Please define the MONGODB_URI environment variable inside .env.local'
+    'Please define the MONGODB_URI environment variable inside .env'
   );
 }
 
@@ -25,10 +25,23 @@ async function dbConnect() {
     };
 
     cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
+      console.log('MongoDB connected successfully.');
       return mongoose;
+    }).catch(err => {
+      console.error('MongoDB connection error:', err.message);
+      // Throw the error to be caught by the calling function
+      throw err;
     });
   }
-  cached.conn = await cached.promise;
+  
+  try {
+    cached.conn = await cached.promise;
+  } catch (e) {
+    // If the promise was rejected, reset it so we can try again on the next request.
+    cached.promise = null;
+    throw e;
+  }
+  
   return cached.conn;
 }
 
